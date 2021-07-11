31aa28e3836d754f1953d26076587a5327ef9974
armanshz <85336907+armanshz@users.noreply.github.com>
2021-07-11 21:30:27 +0800

    Update sheet_scripts.js

--- sheet_scripts.js
+++ sheet_scripts.js
@@ -1,4 +1,5 @@
 function myFunction() {
+  // Logs the values in each cell given a range of cells
   var sheet = SpreadsheetApp.getActiveSheet()
   var rng = sheet.getDataRange();
   var vals = rng.getValues();
@@ -9,4 +10,15 @@ function myFunction() {
 
     })
   })
+}
+
+function myFunction() {
+  // gets the last filled cell in a given column and prompts user 4 times for a number using input box and fills the next filled cell with the number.
+  var sheet = SpreadsheetApp.getActiveSpreadsheet().getActiveSheet();
+  var lastrow  = sheet.getRange("A1").getDataRegion().getNumRows();
+  for (let i = 1; i <5; i++){
+    var input = Browser.inputBox("Input","Enter a number",Browser.Buttons.OK);
+    sheet.getRange(lastrow+i,1).setValue(input);
+  };
+
 }
